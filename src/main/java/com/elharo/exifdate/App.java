4ddbcfb962606482eb0ed122c2421d8bdddd9181
Elliotte Rusty Harold <elharo@ibiblio.org>
2020-07-12 17:51:24 -0400

    move files around

--- src/main/java/com/elharo/exifdate/App.java
+++ src/main/java/com/elharo/exifdate/App.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.elharo.exifdate;
 
 /**
  * Hello world!
--- src/test/java/com/elharo/exifdate/AppTest.java
+++ src/test/java/com/elharo/exifdate/AppTest.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.elharo.exifdate;
 
 import static org.junit.Assert.assertTrue;
 
